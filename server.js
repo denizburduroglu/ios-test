fed6664f3a471212b912ca413f0ed6acb72b6284
Deniz Burduroglu <denizburduroglu@gmail.com>
2021-03-15 22:44:02 -0400

    server.js changes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/OAuthApp'));
+app.use(express.static(__dirname + '/dist/oauth-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/OAuthApp/index.html'));
+res.sendFile(path.join(__dirname+'/dist/oauth-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
